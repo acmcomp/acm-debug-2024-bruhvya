78b2ceb96b747a881b46816af3e199810a712957
Bhavya Kothari <102908917+bruhvya@users.noreply.github.com>
2024-12-05 13:19:25 +0400

    Update Solution.py
    i think done

--- Q6/Solution.py
+++ Q6/Solution.py
@@ -1,14 +1,12 @@
 from flask import Flask, jsonify
-from dataclasses import dataclass
+from dataclasses import dataclass,asdict
 
-
+@dataclass
 class User:
     name: str
     age: int
 
-    def __init__(self, name: str, age: int):
-        self.name = name
-        self.age = age
+
 
 
 app = Flask(__name__)
@@ -17,7 +15,7 @@ app = Flask(__name__)
 @app.route("/data", methods=["GET"])
 def get_data():
     user = User(name="Alice", age=30)
-    return user
+    return jsonify(asdict(user))
 
 
 if __name__ == "__main__":
